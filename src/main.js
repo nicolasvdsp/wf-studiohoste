bea23361f44fc850d5589fcc8c874af42c46c515
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2024-11-25 18:55:19 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { DotLottiePlayer } from 'https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs';
+
+import './scss/app.scss';
+
+console.log("Custom code for Studio hoste");
