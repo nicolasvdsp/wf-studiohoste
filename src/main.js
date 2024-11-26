3e0f268656a452f6ef473373de526f33f78fcb5a
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2024-11-26 13:23:37 +0100

    feat: adds hover distortion effect on project items

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { DotLottiePlayer } from 'https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs';
 
 import './scss/app.scss';
+import distordProjectOnHover from './features/animations';
 
-console.log("Custom code for Studio hoste");
+distordProjectOnHover();
