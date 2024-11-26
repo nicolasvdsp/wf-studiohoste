32197474811d1206e36620f550d0cc9ecc75ac60
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2024-11-26 13:53:06 +0100

    feat: finetunes the hover effect

--- src/features/animations.js
+++ src/features/animations.js
@@ -3,13 +3,15 @@ import hoverEffect from 'hover-effect'
 function distordProjectOnHover() {
   Array.from(document.querySelectorAll(".disp")).forEach((item) => {
     const imgs = Array.from(item.querySelectorAll('img'));
-    let speed = 1.3;
+    let speed = .75;
+    let dispSrc = "https://cdn.prod.website-files.com/673c64d37367ed96ed55b437/6745c04d524317946ce3b3f5_disp4.png";
+
     new hoverEffect({
       parent: item,
-      intensity: 0.3,
+      intensity: 0.1,
       image1: imgs[1].getAttribute('src'),
       image2: imgs[0].getAttribute('src'),
-      displacementImage: 'https://cdn.prod.website-files.com/673c64d37367ed96ed55b437/674518a6d6a4f4c1ceec5ef8_disp2.png',
+      displacementImage: dispSrc,
       speedIn: speed,
       speedOut: speed
     });
