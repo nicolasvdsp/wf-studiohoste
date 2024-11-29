b92de9ba3a7ee821de6221c6ba962049676b6243
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2024-11-30 00:36:10 +0100

    feat: extends the hover effects

--- src/features/animations.js
+++ src/features/animations.js
@@ -1,21 +1,35 @@
 import hoverEffect from 'hover-effect'
 
-function distordProjectOnHover() {
-  Array.from(document.querySelectorAll(".disp")).forEach((item) => {
-    const imgs = Array.from(item.querySelectorAll('img'));
-    let speed = .75;
-    let dispSrc = "https://cdn.prod.website-files.com/673c64d37367ed96ed55b437/6745c04d524317946ce3b3f5_disp4.png";
+function distordItemOnHover(parentClass, dispSrc, isBgImage = false) {
+  Array.from(document.querySelectorAll(`.${parentClass}`)).forEach((item) => {
+    const imgs = isBgImage ?
+      item.querySelector('img').getAttribute('src') :
+      Array.from(item.querySelectorAll('img'));
+
+    let speed = 1.3;
+    console.log(imgs);
 
     new hoverEffect({
       parent: item,
       intensity: 0.1,
-      image1: imgs[1].getAttribute('src'),
-      image2: imgs[0].getAttribute('src'),
+      image1: isBgImage ? imgs : imgs[1].getAttribute('src'),
+      image2: isBgImage ? isBgImage : imgs[0].getAttribute('src'),
       displacementImage: dispSrc,
       speedIn: speed,
       speedOut: speed
     });
   })
+
+  // console.log(isBgImage);
 }
 
-export default distordProjectOnHover;
+function distordItemsOnHover() {
+  let dispSrc = "https://cdn.prod.website-files.com/673c64d37367ed96ed55b437/6745c04d524317946ce3b3f5_disp4.png";
+  let transitionSrc = "https://cdn.prod.website-files.com/673c64d37367ed96ed55b437/674526173907dab7fa665f3d_sand-light.jpg";
+
+  distordItemOnHover("disp", dispSrc);
+  distordItemOnHover("instagram-item", dispSrc);
+}
+
+
+export default distordItemsOnHover;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { DotLottiePlayer } from 'https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs';
 
 import './scss/app.scss';
-import distordProjectOnHover from './features/animations';
+import distordItemsOnHover from './features/animations';
 
-distordProjectOnHover();
+distordItemsOnHover();
