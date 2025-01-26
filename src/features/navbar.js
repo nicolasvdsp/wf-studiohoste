7cb325c7a1e008ac7cc82a092f478c8b96e1464e
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2025-01-26 01:25:31 +0100

    feat: navbar animation - underlines current page

--- src/features/navbar.js
+++ src/features/navbar.js
@@ -6,10 +6,24 @@ function updateAfterWidth() {
   document.documentElement.style.setProperty('--after-width', newWidth);
 }
 
+function currentPage() {
+  const currentPath = window.location.pathname;
+
+  const navLinks = Array.from(document.querySelectorAll('nav a'));
+  // Controleer welke link overeenkomt met de huidige path
+  navLinks.forEach(link => {
+    const href = link.getAttribute('href'); // Haal href van de link
+    if (currentPath === href) {
+      link.classList.add('active'); // Voeg de 'active' klasse toe
+    }
+  });
+}
+
 // Run on page load and whenever the window resizes
 function navbar() {
   window.addEventListener('resize', updateAfterWidth);
   updateAfterWidth();
+  currentPage();
 }
 
 export default navbar;
